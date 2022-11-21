9cbb44fe0ee17b5b38ebedd3cb4fa4ce8ef428ab
autb <bautran+autb+github@gmail.com>
2022-11-21 18:19:50 +0100

    trying azure friday demo for sql emulator & docker &  and azure function emulator

---
+++ functions/Cars.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Azure.Samples
+{
+    public static class Cars
+    {
+        // Returns list of cars
+        [FunctionName("GetCars")]
+        public static IActionResult RunGetCars(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "cars")] HttpRequest req,
+            [Sql("SELECT * FROM dbo.Cars",
+                CommandType = System.Data.CommandType.Text,
+                ConnectionStringSetting = "SqlConnectionString")] IEnumerable<Object> result,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger with SQL Input Binding function processed a request.");
+
+            return new OkObjectResult(result);
+        }
+
+        // Inserts a new car
+        [FunctionName("AddCar")]
+        public static async Task<ObjectResult> RunAddCar(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "cars")] HttpRequest req,
+            [Sql("dbo.Cars", ConnectionStringSetting = "SqlConnectionString")] IAsyncCollector<Car> output,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger with SQL Output Binding function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            try {
+                Car newCar = JsonConvert.DeserializeObject<Car>(requestBody);
+                newCar.Id = Guid.NewGuid();
+                await output.AddAsync(newCar);
+                return new CreatedResult(req.Path, newCar);
+            } catch (Exception ex) {
+                return new BadRequestObjectResult(ex.Message);
+            }
+        }
+    }
+
+    public class Car
+    {
+        // make, model, year, color
+        public Guid? Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ sql/Cars.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[Cars]
+(
+  [Id] [uniqueidentifier] NOT NULL,
+  [Make] [nvarchar](100) NOT NULL,
+  [Model] [nvarchar](100) NOT NULL,
+  [Year] [int] NOT NULL,
+  [Color] [nvarchar](100) NOT NULL,
+
+  PRIMARY KEY CLUSTERED ([Id] ASC)
+)
